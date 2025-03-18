0e030b7663726cd1fe0924a83efc5d78030768a0
YukiRinLL <944989026@qq.com>
2025-03-18 19:11:32 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# 使用 OpenJDK 作为基础镜像
+FROM openjdk:11-jre-slim
+
+# 将 JAR 包复制到容器中
+COPY target/phantoms-backend-1.0-SNAPSHOT.jar /app/phantoms-backend.jar
+
+# 配置容器启动时的命令
+ENTRYPOINT ["java", "-jar", "/app/phantoms-backend.jar"]
+
+# 暴露端口
+EXPOSE 8080
---
+++ src/main/java/com/phantoms/phantomsbackend/PhantomsBackendApplication.java
@@ -0,0 +1,11 @@
+package com.phantoms.phantomsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantomsBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhantomsBackendApplication.class, args);
+    }
+}
