74dbed630f609dafe0ce399ab8840546a63bd135
YukiRinLL <944989026@qq.com>
2025-04-28 16:59:15 +0800

    mapper registerModule jsr310.JavaTimeModule

--- src/main/java/com/phantoms/phantomsbackend/common/config/JacksonConfig.java
+++ src/main/java/com/phantoms/phantomsbackend/common/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package com.phantoms.phantomsbackend.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.phantoms.phantomsbackend.common.serializer.JsonArraySerializer;
 import co.casterlabs.rakurai.json.element.JsonArray;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +13,13 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+
         SimpleModule module = new SimpleModule();
         module.addSerializer(JsonArray.class, new JsonArraySerializer());
         mapper.registerModule(module);
+
+        mapper.registerModule(new JavaTimeModule());
+
         return mapper;
     }
 }
