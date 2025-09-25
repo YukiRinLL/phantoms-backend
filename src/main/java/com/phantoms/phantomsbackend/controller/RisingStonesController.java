ab6d419129fe9001422f46519fdc9f204957f480
YukiRinLL <944989026@qq.com>
2025-09-25 11:26:24 +0800

    add updateDaoYuKey API

--- src/main/java/com/phantoms/phantomsbackend/controller/RisingStonesController.java
+++ src/main/java/com/phantoms/phantomsbackend/controller/RisingStonesController.java
@@ -2,6 +2,7 @@ package com.phantoms.phantomsbackend.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.phantoms.phantomsbackend.service.RisingStonesService;
+import com.phantoms.phantomsbackend.service.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ public class RisingStonesController {
 
     @Autowired
     private RisingStonesService risingStonesService;
+
+    @Autowired
+    private SystemConfigService systemConfigService;
 
     @GetMapping("/user-info")
     public JSONObject userInfo(@RequestParam String uuid) throws IOException {
@@ -27,5 +31,20 @@ public class RisingStonesController {
     @GetMapping("/guild-member")
     public JSONObject guildMember(@RequestParam(value = "guildId", defaultValue = "9381138761301105564") String guildId) throws IOException {
         return risingStonesService.getGuildMember(guildId);
+    }
+
+    // 更新daoyu_key密钥
+    @PostMapping("/update-daoyu-key")
+    public JSONObject updateDaoYuKey(@RequestParam String newKey) {
+        JSONObject result = new JSONObject();
+        try {
+            systemConfigService.updateDaoYuKey(newKey);
+            result.put("success", true);
+            result.put("message", "daoyu_key更新成功");
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("message", "daoyu_key更新失败: " + e.getMessage());
+        }
+        return result;
     }
 }
