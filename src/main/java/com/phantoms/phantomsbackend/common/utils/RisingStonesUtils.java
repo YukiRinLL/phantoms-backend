1d828d6033ab078753bf28293acbccf9d7aaa871
YukiRinLL <944989026@qq.com>
2025-12-31 10:41:10 +0800

    fix RisingStonesUtils as Component

--- src/main/java/com/phantoms/phantomsbackend/common/utils/RisingStonesUtils.java
+++ src/main/java/com/phantoms/phantomsbackend/common/utils/RisingStonesUtils.java
@@ -4,6 +4,7 @@ import okhttp3.*;
 import com.alibaba.fastjson.JSONObject;
 import com.phantoms.phantomsbackend.service.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.*;
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Component
 public class RisingStonesUtils {
 
     private static final Logger logger = Logger.getLogger(RisingStonesUtils.class.getName());
